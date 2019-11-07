48dfd1672fb59c9beaa891fa9263b2cbabd15f40
Frank <frank@elsinga.de>
2019-11-07 14:47:30 +0100

    minor reformating and commenting

--- Import_to_JSON.py
+++ Import_to_JSON.py
@@ -5,17 +5,23 @@ import mysql.connector
 
 def importToJSON(p):
     jsondaten = 'data.txt'
+    # delete current data
     open(jsondaten, 'w').close()
-    mydb = mysql.connector.connect(user='root', password=p,
+
+    # SQL-init
+    mydb = mysql.connector.connect(user='root',
+                                   password=p,
                                    host='127.0.0.1')
     mycursor = mydb.cursor()
-
+    # getting data from SQL
     mycursor.execute(
         "SELECT BootsID, RennID, Bootsname, SteuerlingID, Athlet1ID, Athlet2ID, Athlet3ID, Athlet4ID, Athlet5ID, "
         "Athlet6ID, Athlet7ID, Athlet8ID FROM boote.boote")
     myresult = mycursor.fetchall()
     mycursor.execute("UPDATE boote.processing SET StateHasChangedSinceLastImport = 0")
     mydb.close()
+
+    # generating boote
     data = {'boote': []}
     for x in myresult:
         boots_id, renn_id, bootsname, steuerling_id, athlet1ID, athlet2ID, athlet3ID, athlet4ID, athlet5ID, athlet6ID, athlet7ID, athlet8ID = x
@@ -27,6 +33,7 @@ def importToJSON(p):
                          athlet8ID]
         })
 
+    # generating vertecies
     vertecies = dict()
     for verteciestmp in data['boote']:
         if not verteciestmp['renn_id'] in vertecies.keys():
@@ -36,7 +43,8 @@ def importToJSON(p):
         vertecies[verteciestmp['renn_id']]['bootsname'].append(verteciestmp['bootsname'])
         for athletennametmp in verteciestmp['athleten']:
             vertecies[verteciestmp['renn_id']]['athleten'].append(athletennametmp)
-
     data['vertecies'] = vertecies
+
+    # saving progress
     with open(jsondaten, 'w') as outfile:
         json.dump(data, outfile)
